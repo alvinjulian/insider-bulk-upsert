406153a0bc7ae4b493a965cb4d95648993a8e0b0
Alvin Julian <alvin.julian@outlook.com>
2024-01-12 22:00:15 +0700

    adding new feature to combine json
    combine json from parent folder
    push more at the same time
    not tested yet

--- index.js
+++ index.js
@@ -6,76 +6,134 @@ const path = require('path');
 const axios = require('axios');
 const Bottleneck = require('bottleneck');
 
-// Change the folder name for json inputs
-const inputsDir = path.join(__dirname, 'inputs/files/20231214/files_091');
+// Add new dependency for folder scanner 
+const {readdir} = require('fs/promises');
+
+// Async function to read the whole directory
+async function listDirectories(pth) {
+  const directories = (await readdir(pth, {withFileTypes: true}))
+    .filter(dirent => dirent.isDirectory())
+    .map(dir => dir.name);
+
+  return directories;
+}
+
+// Parent directory or folder for the inputs
+const parentDir = './inputs'
+
+// function to check path is empty
+function isEmpty(path) {
+    return fs.readdirSync(path).length === 0;
+}
 
 // Declare limiter for bottlenecking activity
 // Change the maxConcurrent and minTime values for your needs
 const limiter = new Bottleneck({
-    maxConcurrent: 1,
+    // max concurrent requests
+    maxConcurrent: 10,
+    // min time to wait between each request
     minTime: 150
 });
 
 var i = 0;
 
-fs.readdir(inputsDir, (err, files) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
+// variable to handle the payload limit within one request - you can customize this value based on your needs and size of the data in one json file
+const _limitPayload = 10;
 
-    files.forEach((file) => {
-        limiter.schedule(() => {
-            if(!file.endsWith('.json')) {
+// variable to handle the combined data
+var _combinedData = [];
+
+listDirectories(parentDir).then(directories => {
+    directories.forEach((directory, index) => {
+        // Change the folder name for json inputs
+        const inputsDir = path.join(parentDir, directory);
+
+        // Run the read files from each folder
+        fs.readdir(inputsDir, (err, files) => {
+
+            // Check any error that occured
+            if (err) {
+                console.error(err);
                 return;
             }
 
-            const filePath = path.join(inputsDir, file);
-
-            // Check the filepath
-            console.log(filePath)
-            
-            // Read the file content and slide the last 12 characters
-            const fileContent = fs.readFileSync(filePath, 'utf8');
-            const slicedContent = fileContent.slice(0, -12);
-
-            console.log(slicedContent)
-
-            // Parse the file content to JSON
-            const jsonContent = JSON.parse(slicedContent);
-
-            console.log(jsonContent)
-
-            i++;
-            console.log(i);
-
-            // Upsert API URL
-            const url = 'https://unification.useinsider.com/api/user/v1/upsert'
-
-            // Change the partner name and request token
-            const partnerName = ''
-            const requestToken = ''
+            // Check whether the inputsDir is empty or not
+            if(isEmpty(inputsDir)) {
+                console.error(err);
+                return;
+            }
         
-            axios({
-                method: 'POST',
-                url: url,
-                headers: {
-                    'X-PARTNER-NAME': partnerName,
-                    'X-REQUEST-TOKEN': requestToken,
-                    'Content-Type': 'application/json'
-                },
-                data: jsonContent
-            })
-            .then(response => {
-                if (response.status > 200 && response.status < 300) {
-                    console.log(response.status)
-                    console.log(response.data)
-                }
-            })
-            .catch(error => {
-                console.log(error.response.status)
-                console.log(error.response.data)
-            })
-        })
-    });
+            // Run the automation within the folder to ingest the data to Upsert API
+            files.forEach((file) => {
+                limiter.schedule(() => {
+                    if(!file.endsWith('.json')) {
+                        return;
+                    }
+
+                    if(_combinedData.length < _limitPayload) {
+                        const filePath = path.join(inputsDir, file);
+    
+                        // Check the filepath
+                        console.log(filePath)
+                        
+                        // Read the file content and slide the last 12 characters - Comment these two lines below if you don't need it.
+                        const fileContent = fs.readFileSync(filePath, 'utf8');
+                        const slicedContent = fileContent.slice(0, -12);
+            
+                        console.log(slicedContent)
+            
+                        // Parse the file content to JSON
+                        var jsonContent;
+
+                        try {
+                            jsonContent = JSON.parse(slicedContent);
+                            _combinedData.push(jsonContent);
+                        } catch (error) {
+                            i++;
+                            console.log('JSON Data cannot be parsed row' + i + ' - under filename: ' + filePath)
+                        }
+            
+                        console.log(_combinedData)
+            
+                        i++
+                        console.log(i);
+                    } else if (_combinedData.length === _limitPayload) {
+                        // Run the upsert API Calls after the combinedData has reached the limitPayload
+                        // Upsert API URL
+                        const url = 'https://unification.useinsider.com/api/user/v1/upsert'
+            
+                        // Change the partner name and request token
+                        const partnerName = ''
+                        const requestToken = ''
+
+                        console.log(_combinedData)
+                    
+                        axios({
+                            method: 'POST',
+                            url: url,
+                            headers: {
+                                'X-PARTNER-NAME': partnerName,
+                                'X-REQUEST-TOKEN': requestToken,
+                                'Content-Type': 'application/json'
+                            },
+                            data: _combinedData
+                        })
+                        .then(response => {
+                            if (response.status > 200 && response.status < 300) {
+                                console.log(response.status)
+                                console.log(response.data)
+
+                                // Reset the combinedData
+                                _combinedData = [];
+                            }
+                        })
+                        .catch(error => {
+                            console.log(error.response.status)
+                            console.log(error.response.data)
+                        })
+                    }
+                })
+            });
+        });
+    })
 });
